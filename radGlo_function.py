125fc330a0055ef76e8637a76caa60923d0cda8a
Pedro Lopes Maia <34479719+PedroLopesMaia@users.noreply.github.com>
2021-07-08 19:11:22 -0300

    Add files via upload

---
+++ radGlo_function.py
@@ -0,0 +1,11 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # TODO implement
+    s3_client = boto3.client('s3')
+    s3_clientobj = s3_client.get_object(Bucket='weather-pernambuco2', Key='data.json')
+    s3_clientdata = s3_clientobj['Body'].read().decode('utf-8')
+    s3clientlist=json.loads(s3_clientdata)
+    response = s3clientlist["RAD_GLO"]
+    return { 'VALOR_OBSERVADO': response }
